d308898a02a732fd72b3730d4f57281b127ff229
Your Name <hinnehmorris4@gmail.com>
2022-10-25 13:47:32 -0600

    sendtext

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState, } from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity, AsyncStorage } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, AsyncStorage, TextInput, Button} from 'react-native';
 import  Navigation from './components/Navigation';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import OnboardingScreen from './screens/OnboardingScreen';
 import Home from './screens/Home';
 import { NavigationContainer } from '@react-navigation/native';
+import { createKeyboardAwareNavigator } from 'react-navigation';
 
 
 
@@ -13,16 +14,73 @@ const AppStack = createNativeStackNavigator();
 
 const App = () =>{
   const [isFirstLaunch, setFirstLaunch] = React.useState(true);
-  const [homeTodayScore, setHomeTodayScore] = React.useState(0);
+  const [phoneNumber,setphoneNumber] = React.useState(false); 
+  const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+  //edit in the line
 
    if (isFirstLaunch == true){
 return(
   <OnboardingScreen setFirstLaunch={setFirstLaunch}/>
  
 );
-  }else{
+  }else if(isLoggedIn){ //start here
     return <Navigation/>
   }
+   else {
+ return(
+     <View> 
+      
+       <TextInput
+       value={phoneNumber}
+       onChangeText={phoneNumber}
+       style={styles.input}
+       placeholderTextColor= '#4251f5'
+       placeholder='Cell phone'>
+      </TextInput>
+      <Button
+      title='Send'
+      style={styles.button}
+      onPress={async()=>{
+        console.log(phoneNumber +'Button was pressed')
+        await fetch(
+          'https://dev.stedi.me/twofactorlogin/8013862433',
+          {
+            method: 'POST',
+            headers:{
+            "content-type":'application/text'
+            }
+          })
+
+      }}
+      />
+      </View>
+)// send edit here
+
 }
- export default App;
+}
+export default App;
+      
+const styles = StyleSheet.create({
+  container:{
+      flex:1, 
+      alignItems:'center',
+      justifyContent: 'center'
+  },
+  input: {
+    marginTop: 100,
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  margin:{
+    marginTop:100 //add manually
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "#DDDDDD",
+    padding: 10
+  }    
+})
+
 
--- screens/OnboardingScreen.js
+++ screens/OnboardingScreen.js
@@ -44,11 +44,11 @@ const OnboardingScreen = ({setFirstLaunch}) =>{
               backgroundColor: '#fff',
               image: <Image   style={{height: '60%', width: '90%', resizeMode:'contain', marginBottom:-200}} source={require('../image/testing.png')} />,
               title: <Text style={{fontWeight:'bold', textAlign:'center', color:'#A0CE4E', fontSize:19, margin:12,  marginTop: -70}}>Welcome to STEDI  Balance</Text>,
-              subtitle: ( 
-               <View>
+              subtitle: 'senttext'
+               
 
-              </View>
-              )
+            
+              
             },
             {
                 backgroundColor: '#fff',
